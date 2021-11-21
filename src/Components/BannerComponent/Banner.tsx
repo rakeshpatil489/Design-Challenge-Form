729595f5abb7fbac7b6f4a908922dfdb662c97ec
rakeshpatil <rakeshpatil489@gmail.com>
2021-11-21 20:21:57 +0100

    Updates on Styling

--- src/Components/BannerComponent/Banner.tsx
+++ src/Components/BannerComponent/Banner.tsx
@@ -4,8 +4,8 @@ import styles from './Banner.module.css';
 function Banner() {
     return (
         <div className={styles.bannerContainer}>
-            <div>
-                <h1 className={styles.jobTitle}>UX Designer (m/f/d)</h1>
+            <div className={styles.bannerWrapper}>
+                <h2 className={styles.jobTitle}>UX Designer (m/f/d)</h2>
                 <p className={styles.jobSubtitle}>Fulltime, Berlin, Germany</p>
             </div>
         </div>
--- src/Components/FormComponent/Form.tsx
+++ src/Components/FormComponent/Form.tsx
@@ -10,8 +10,8 @@ function Form() {
     <form className={styles.container}>
       <div className={styles.formWrapper}>
         {/* title of the form   */}
-        <div className={styles.headingTitle}>
-          <h2>Job Application</h2>
+        <div className={styles.headingTitleContainer}>
+          <h2 className={styles.headingTitle}>Job Application</h2>
           <label className={styles.helperText}>
             <span className={styles.required}>*</span> Required
           </label>
@@ -27,10 +27,13 @@ function Form() {
         </div>
 
         <div className={styles.uploadContainer}>
-          <h3 className={styles.uploadHeading}>Upload Documents</h3>
-          <label className={styles.helperText}>
-            File accepcted - .PDF, .docs/docx - Max file size 20kb for each
-          </label>
+
+          <div className={styles.uploadHeadingContainer}>
+            <h3 className={styles.uploadHeading}>Upload Documents</h3>
+            <label className={styles.helperText}>
+              File accepcted - .PDF, .docs/docx - Max file size 20kb for each
+            </label>
+          </div>
 
           {/* Upload Fields */}
           <Upload />

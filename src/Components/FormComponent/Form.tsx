24ed416fd481c8350fba71fd1b559984b0a4b09e
rakeshpatil <rakeshpatil489@gmail.com>
2021-11-21 17:51:29 +0100

    Upload File Component

--- src/Components/FormComponent/Form.tsx
+++ src/Components/FormComponent/Form.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import InputField from "../InputfieldComponent/InputField";
+import Upload from "../UploadComponent/Upload";
 import Button from "../ButtonComponent/Button";
 import styles from "./Form.module.css";
 
 function Form() {
   return (
     <form>
-
       {/* title of the form   */}
       <div className={styles.headingTitle}>
         <h2>Job Application</h2>
@@ -15,21 +15,26 @@ function Form() {
         </label>
       </div>
 
-
       <div className={styles.inputfieldContainer}>
         <h3 className={styles.inputfieldHeading}>Personal Info</h3>
+
         {/* Inputfields */}
         <InputField />
         <InputField />
         <InputField />
-      </div>
 
+      </div>
 
       <div className={styles.uploadContainer}>
         <h3 className={styles.uploadHeading}>Upload Documents</h3>
         <label className={styles.helperText}>
           File accepcted - .PDF, .docs/docx - Max file size 20kb for each
         </label>
+
+        {/* Upload Fields */}  
+        <Upload />
+        <Upload />
+
       </div>
 
       {/* Divider */}
---
+++ src/Components/UploadComponent/Upload.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from "./Upload.module.css";
+
+function Upload() {
+    return (
+        <div className={styles.uploadFieldContainer}>
+            <label className={styles.uploadFieldLabel}> Label <span className={styles.required}>*</span></label>
+            <div className={styles.uploadField}>
+                <span>Choose a file</span> (or) Drop a file.
+            </div>
+        </div>
+    )
+}
+
+export default Upload
+
